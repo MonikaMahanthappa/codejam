b8ce9e6922720afab712d2b375a0e065fc6cb2f8
MonikaMahanthappa <monika@mavenhive.in>
2014-04-11 22:55:49 +0530

    add skeleton

---
+++ problem1/solution.rb
@@ -0,0 +1,15 @@
+i_dir = '/Users/mavenhive/Public/codejam/problem1/inputs'
+o_dir = '/Users/mavenhive/Public/codejam/problem1/outputs'
+output = []
+inputs = File.readlines(i_dir + '/A-large-practice.in').drop(1).each_slice(3).to_a
+inputs.each do |credit, length, inps|
+
+end
+
+#Write output to file
+File.open(o_dir + '/A-large-practice.out', 'w+') do |file|
+  output.each_slice(2).to_a.each_with_index do |element, index|
+    index += 1
+    file.puts('Case #'+ index.to_s + ':  ' + element.join(' '))
+  end
+end
