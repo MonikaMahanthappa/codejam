0ea2a7435d690074e408d988e0bca629243d4124
MonikaMahanthappa <monika@mavenhive.in>
2014-04-12 21:35:42 +0530

    play game with war approach

--- deceitful_war/solution.rb
+++ deceitful_war/solution.rb
@@ -1,10 +1,62 @@
+class Player
+  def initialize(input)
+    @woods = input.split(' ').sort_by {|i| i.to_f}
+    @points = 0
+  end
+
+  attr_reader :points
+  attr_accessor :woods
+
+  def closest_value(target)
+    ceiling_value = nil
+    @woods.each do  |wood|
+      if wood > target
+        ceiling_value = wood
+        break
+      end
+    end
+    ceiling_value
+  end
+
+  def inc_points
+    @points += 1
+  end
+
+  def burn_wood(wood)
+    @woods.delete(wood)
+  end
+end
+
+class War
+  def war(inputs)
+    outputs = []
+    inputs.each do |number_of_woods, naomi_woods, ken_woods|
+      naomi = Player.new(naomi_woods)
+      ken = Player.new(ken_woods)
+      naomi.woods.each do |naomi_wood|
+        ken_wood = ken.closest_value(naomi_wood)
+        if ken_wood
+          ken.inc_points
+        else
+          naomi.inc_points
+        end
+        ken.burn_wood(ken_wood)
+      end
+      outputs.push(naomi.points)
+    end
+    outputs
+  end
+end
+
+
 i_dir = '/Users/mavenhive/Public/codejam/deceitful_war/inputs'
 o_dir = '/Users/mavenhive/Public/codejam/deceitful_war/outputs'
 output = []
-inputs = File.readlines(i_dir + '/A-large-practice.in').drop(1).each_slice(3).to_a
-inputs.each do |credit, length, inps|
+original_inputs = File.readlines(i_dir + '/A-test-practice.in').drop(1).each_slice(3).to_a
+war = War.new
+output = war.war(original_inputs)
+puts output
 
-end
 
 #Write output to file
 #File.open(o_dir + '/A-large-practice.out', 'w+') do |file|
