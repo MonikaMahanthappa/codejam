b25556c05ac27edc66871d596946ca27b7030f9b
MonikaMahanthappa <monika@mavenhive.in>
2014-04-12 11:44:51 +0530

    first

---
+++ magic_trick/solution.rb
@@ -0,0 +1,15 @@
+i_dir = '/Users/mavenhive/Public/codejam/magic_trick/inputs'
+o_dir = '/Users/mavenhive/Public/codejam/magic_trick/outputs'
+output = []
+inputs = File.readlines(i_dir + '/A-small-practice.in').drop(1).each_slice(10).to_a
+inputs.each do |credit, length, inps|
+
+end
+
+#Write output to file
+#File.open(o_dir + '/A-small-practice.out', 'w+') do |file|
+#  output.each_slice(2).to_a.each_with_index do |element, index|
+#    index += 1
+#    file.puts('Case #'+ index.to_s + ':  ' + element.join(' '))
+#  end
+#end
