51c8c4b695757391785fb5a74c861d2c3937e44a
MonikaMahanthappa <monika@mavenhive.in>
2014-04-12 23:37:48 +0530

    deceitful war final solution

--- deceitful_war/solution.rb
+++ deceitful_war/solution.rb
@@ -1,10 +1,11 @@
 class Player
   def initialize(input)
-    @woods = input.split(' ').sort_by {|i| i.to_f}
+    @woods = input.split(' ').sort_by {|i| i.to_f}.map { |x| x.to_f }
+    @deciteful_woods = []
     @points = 0
   end
 
-  attr_reader :points
+  attr_reader :points, :deciteful_woods
   attr_accessor :woods
 
   def closest_value(target)
@@ -24,6 +25,12 @@ class Player
 
   def burn_wood(wood)
     @woods.delete(wood)
+  end
+
+  def populate_cheated_values
+    first_element = @woods.first
+    @deciteful_woods = @woods.drop(1)
+    @deciteful_woods.push(first_element)
   end
 end
 
@@ -46,22 +53,41 @@ class War
     end
     outputs
   end
+
+  def deceitful_war(inputs)
+    outputs = []
+    inputs.each do |number_of_woods, naomi_woods, ken_woods|
+      naomi = Player.new(naomi_woods)
+      ken = Player.new(ken_woods)
+      naomi.populate_cheated_values
+      naomi.deciteful_woods.each_with_index do |naomi_wood, index|
+        ken_wood = ken.woods[index]
+        if ken_wood > naomi_wood
+          ken.inc_points
+        else
+          naomi.inc_points
+        end
+      end
+      outputs.push(naomi.points)
+    end
+    outputs
+  end
 end
 
 
 i_dir = '/Users/mavenhive/Public/codejam/deceitful_war/inputs'
 o_dir = '/Users/mavenhive/Public/codejam/deceitful_war/outputs'
-output = []
 original_inputs = File.readlines(i_dir + '/A-test-practice.in').drop(1).each_slice(3).to_a
 war = War.new
-output = war.war(original_inputs)
-puts output
+honest_war_output = war.war(original_inputs)
+deceitful_war_output = war.deceitful_war(original_inputs)
 
 
 #Write output to file
-#File.open(o_dir + '/A-large-practice.out', 'w+') do |file|
-#  output.each_slice(2).to_a.each_with_index do |element, index|
-#    index += 1
-#    file.puts('Case #'+ index.to_s + ':  ' + element.join(' '))
-#  end
-#end
+File.open(o_dir + '/A-test-practice.out', 'w+') do |file|
+index = 0
+  deceitful_war_output.zip(honest_war_output).each do |cheated_point, honest_point|
+    index += 1
+    file.puts('Case #'+ index.to_s + ':  ' + cheated_point.to_s + ' ' + honest_point.to_s)
+  end
+end
