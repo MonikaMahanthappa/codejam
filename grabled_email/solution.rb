a5c9a86ba2add9d5ec3e906e1a1cf7f5cd57afcb
MonikaMahanthappa <monika@mavenhive.in>
2014-05-04 12:43:30 +0530

    add charging chaos problem

--- grabled_email/solution.rb
+++ grabled_email/solution.rb
@@ -10,7 +10,7 @@ end
 File.open(o_dir + '/A-large-practice.out', 'w+') do |file|
   output.each_slice(2).to_a.each_with_index do |element, index|
     index += 1
-    file.puts('Case #'+ index.to_s + ':  ' + element.join(' '))
+    file.puts('Case #'+ \index.to_s + ':  ' + element.join(' '))
   end
 end
 
